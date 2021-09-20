025346584ebb115fbf34ff819a7ef47100ac87bf
Hiroki Furusho <furusityofu@gmail.com>
2021-09-21 06:18:00 +0900

    fishの設定ファイルを追加

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+set HOMEBREW_DIR_A '/opt/homebrew'
+set HOMEBREW_DIR_I '/usr/local'
+function armbrew --wraps brew
+    set PATH $HOMEBREW_DIR_A/bin /usr/bin /bin $HOMEBREW_DIR_A/sbin /usr/sbin /sbin
+    $HOMEBREW_DIR_A/bin/brew $argv
+end
+function intelbrew --wraps brew
+    set PATH $HOMEBREW_DIR_I/bin /usr/bin /bin $HOMEBREW_DIR_I/sbin /usr/sbin /sbin
+    arch --x86_64 $HOMEBREW_DIR_I/bin/brew $argv
+end
+function e
+    emacsclient -nw -a ""
+end
+function ekill
+emacsclient -e "(kill-emacs)"
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
