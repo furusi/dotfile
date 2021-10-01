f374cbe1545899a2f5593a63c961a0a2b339af4a
Hiroki Furusho <furusityofu@gmail.com>
2021-10-01 22:30:15 +0900

    fishの設定を変更

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,13 @@
 set HOMEBREW_DIR_A '/opt/homebrew'
 set HOMEBREW_DIR_I '/usr/local'
 set GOPATH $HOME/go/
+set EDITOR 'emacs'
 function armbrew --wraps brew
-    set PATH $HOMEBREW_DIR_A/bin /usr/bin /bin $HOMEBREW_DIR_A/sbin /usr/sbin /sbin
+    set -l PATH $HOMEBREW_DIR_A/bin /usr/bin /bin $HOMEBREW_DIR_A/sbin /usr/sbin /sbin
     $HOMEBREW_DIR_A/bin/brew $argv
 end
 function intelbrew --wraps brew
-    set PATH $HOMEBREW_DIR_I/bin /usr/bin /bin $HOMEBREW_DIR_I/sbin /usr/sbin /sbin
+    set -l PATH $HOMEBREW_DIR_I/bin /usr/bin /bin $HOMEBREW_DIR_I/sbin /usr/sbin /sbin
     arch --x86_64 $HOMEBREW_DIR_I/bin/brew $argv
 end
 function e
