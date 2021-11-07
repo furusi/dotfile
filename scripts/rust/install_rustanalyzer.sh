b3d5712c56705fcad43c6624b06331ca394f24dc
Hiroki Furusho <furusityofu@gmail.com>
2021-11-07 22:29:50 +0900

    スクリプトを修正

--- scripts/rust/install_rustanalyzer.sh
+++ scripts/rust/install_rustanalyzer.sh
@@ -8,8 +8,14 @@ if [ "$(uname)" == 'Darwin' ]; then
     fi
     OS=$ARCH-apple-darwin
     echo $OS
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    OS='x86_64-unknown-linux-gnu'
+elif [ "$(uname)" == 'Linux' ]; then
+    if [ "$(uname -m)" == "x86_64" ]
+    then
+        ARCH="x86_64"
+    else
+        ARCH="aarch64"
+    fi
+    OS=$ARCH-unknown-linux-gnu
     echo $OS
 else
     echo "Your platform ($(uname -a)) is not supported."
