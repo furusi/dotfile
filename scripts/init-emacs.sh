964e09ca30586dd5d635c3d7ffd6c41869111d01
Hiroki Furusho <furusityofu@gmail.com>
2021-12-14 15:01:34 +0900

    emacsの設定スクリプトで.emacs.dフォルダを生成

--- scripts/init-emacs.sh
+++ scripts/init-emacs.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-cd `dirname $0`                 # ファイルのある場所をカレントディレクトリにする
 EMACS_CONFIG_DIR=$HOME/.emacs.d/
+
+mkdir -p $EMACS_CONFIG_DIR
+cd `dirname $0`                 # ファイルのある場所をカレントディレクトリにする
 ln -s $PWD/../.emacs.d/init.el $EMACS_CONFIG_DIR
 ln -s $PWD/../.emacs.d/early-init.el $EMACS_CONFIG_DIR
 ln -s $PWD/../.emacs.d/snippets $EMACS_CONFIG_DIR
