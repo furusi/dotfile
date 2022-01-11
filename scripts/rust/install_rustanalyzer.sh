10523c6eb2128e4b683c5cb0c8bbe56db9013296
Hiroki Furusho <furusityofu@gmail.com>
2022-01-11 18:20:57 +0900

    install_rustanalyzer.shを修正

--- scripts/rust/install_rustanalyzer.sh
+++ scripts/rust/install_rustanalyzer.sh
@@ -7,7 +7,6 @@ if [ "$(uname)" == 'Darwin' ]; then
         ARCH="aarch64"
     fi
     OS=$ARCH-apple-darwin
-    echo $OS
 elif [ "$(uname)" == 'Linux' ]; then
     if [ "$(uname -m)" == "x86_64" ]
     then
@@ -16,10 +15,11 @@ elif [ "$(uname)" == 'Linux' ]; then
         ARCH="aarch64"
     fi
     OS=$ARCH-unknown-linux-gnu
-    echo $OS
 else
     echo "Your platform ($(uname -a)) is not supported."
     exit 1
 fi
+echo "system: $OS"
+echo "downloading: https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$OS.gz"
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$OS.gz | gunzip -c - > ~/.local/bin/rust-analyzer
 chmod +x ~/.local/bin/rust-analyzer
