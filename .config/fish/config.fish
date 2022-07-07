3d289188b8f8e1c0425c57cb840761c5ca3a8f1a
Hiroki Furusho <furusityofu@gmail.com>
2022-07-07 12:00:33 +0900

    set ghcup's environment variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -54,3 +54,5 @@ end
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH # ghcup-env
