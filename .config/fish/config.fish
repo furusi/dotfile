e76fbf9edcad816379987109ea20735e5ed0a447
Hiroki Furusho <furusityofu@gmail.com>
2023-06-28 10:04:43 +0900

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,26 +6,33 @@ set -gx EDITOR 'emacs'
 set MY_ARCH (uname -m)
 set MY_OS (uname -s)
 
+
+
 #mac
 if test $MY_OS = "Darwin"
-    eval (/usr/local/bin/brew shellenv)
-    eval (/opt/homebrew/bin/brew shellenv)
-    set -gx INFOPATH "/opt/homebrew/share/info/emacs" $INFOPATH "/usr/share/info"
-    set -gx MANPATH /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/share/man $MANPATH
+    # echo "Current Architecture: $MY_ARCH"
+    if status --is-login
+        set -gx INFOPATH "/usr/share/info" $INFOPATH 
+        set -gx MANPATH /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/share/man $MANPATH
+    end
     # arm mac
     if test $MY_ARCH = "arm64"
         set -gx BEERHALL $HOME/BeerHall
-        # homebrew
-        function armbrew --wraps brew
-            set -lx PATH $HOMEBREW_DIR_A/bin /usr/bin /bin $HOMEBREW_DIR_A/sbin /usr/sbin /sbin
-            $HOMEBREW_DIR_A/bin/brew $argv
-        end
-        function intelbrew --wraps brew
-            set -lx PATH $HOMEBREW_DIR_I/bin /usr/bin /bin $HOMEBREW_DIR_I/sbin /usr/sbin /sbin
-            arch --x86_64 $HOMEBREW_DIR_I/bin/brew $argv
-        end
+        eval (/opt/homebrew/bin/brew shellenv)
+    else
+        eval (/usr/local/bin/brew shellenv)
     end
 end
+
+function arm
+    arch -arch arm64 /usr/bin/env fish --login
+end
+
+function x64
+    arch -arch x86_64 /usr/bin/env fish --login
+end
+
+
 
 if command -v starship > /dev/null
     starship init fish | source
@@ -43,7 +50,7 @@ function e
     emacsclient -nw -a ""
 end
 function ekill
-emacsclient -e "(kill-emacs)"
+    emacsclient -e "(kill-emacs)"
 end
 function cd-git-rootdir
     cd (git rev-parse --show-toplevel)
@@ -56,20 +63,20 @@ end
 abbr --add dotdot --regex '^\.\.+$' --function multicd
 
 # opam
-if [ -f $HOME/.opam/opam-init/init.fish ]
+if [ -f $HOME/.opam/opam-init/init.fish ] && status --is-login
     source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 end
 
 # emacs
 set -gx LSP_USE_PLISTS true
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH # ghcup-env
-
 # starship(prompt)
 set -gx STARSHIP_CONFIG $HOME/.config/starship/config.toml
 
-eval "$(rbenv init -)"
+if status --is-login
+    set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH # ghcup-env
+    eval "$(rbenv init -)"
+end
+
+
+pyenv init - | source
