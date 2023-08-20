abebfd4f1abad04faa99bfdc0ededdaf60585389
Hiroki Furusho <furusityofu@gmail.com>
2023-08-20 11:35:18 +0900

    delete binary after binary downloading

--- scripts/rust/install_rustanalyzer.sh
+++ scripts/rust/install_rustanalyzer.sh
@@ -20,12 +20,17 @@ else
     exit 1
 fi
 echo "system: $OS"
+
+echo "downloading: https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$OS.gz"
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$OS.gz | gunzip -c - > ~/.local/bin/rust-analyzer_new
+
+
 if [ -f ~/.local/bin/rust-analyzer ]
 then
-    echo "delete existing rust-analyzer binary"
+    echo "delete old rust-analyzer"
     rm ~/.local/bin/rust-analyzer
 fi
 
-echo "downloading: https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$OS.gz"
-curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$OS.gz | gunzip -c - > ~/.local/bin/rust-analyzer
-chmod +x ~/.local/bin/rust-analyzer
+echo "install new rust-analyzer"
+chmod +x ~/.local/bin/rust-analyzer_new
+mv ~/.local/bin/rust-analyzer_new ~/.local/bin/rust-analyzer
